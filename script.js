549a6a2a3c2cb95acdda111094217c041423e914
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-31 01:26:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+const url= document.querySelector("#url");
+const form= document.querySelector("form");
+const name= document.querySelector("#name");
+const year= document.querySelector("#year");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	if(name.value && year.value) {
+		url.innerText= "https://localhost:8080/" + "?" + "name=" + name.value + "&" + "year=" +  year.value;
+	}
+	else if(name.value) {
+		url.innerText= "https://localhost:8080/" + "?" + "name=" + name.value;	
+	}
+	else if(year.value) {
+		url.innerText= "https://localhost:8080/" + "?" + "year=" + year.value;	
+	}
+	else {
+		url.innerText= "https://localhost:8080/";
+	}
+})
